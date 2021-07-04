28d37a205141b7779fb040d7932a940c51d7ee85
dawit12123 <dawitgirma71@gmail.com>
2021-07-04 11:34:05 -0700

    c hello world

--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -1,6 +1,5 @@
-#include<stdio.h>
+#include <stdio.h>
 
-<<<<<<< HEAD
 /**
  * main - Entry point
  *
@@ -9,12 +8,5 @@
 
 int main(void)
 {
-
 return (0);
-
-=======
-int main (){
-
-return 0;
->>>>>>> a8f094d3bfd56a041a425d2f01ef8e09c83ffca3
 }
