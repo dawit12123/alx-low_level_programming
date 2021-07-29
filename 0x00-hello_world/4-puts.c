75e1a725bbaf20d18918409dd0c3ec81914db5c7
dawit12123 <dawitgirma71@gmail.com>
2021-07-29 06:50:27 -0700

    C Programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
 /** 
- * main : Entry point
- * prints using puts function 
- * Return: 0
+* main : Entry point
+*
+* prints using puts function 
+*
+* Return: 0
  */
-int main()
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-return 0; 
+return (0); 
 }
